88fec2907a6d5f95d71141f7de0f6a74e3b2e6e6
ericliu0601 <ericliu0601@gmail.com>
2025-12-11 23:07:26 +0800

    加入新玩法

--- src/data.js
+++ src/data.js
@@ -24,7 +24,7 @@ export const INITIAL_DATA = [
     "thumbnail": "",
     "difficulty": "中等",
     "fun": "高",
-    "author": "ArenaMaster",
+    "author": "隨機單中：大混戰集錦",
     "stats": {
       "difficulty": "中等",
       "fun": "高"
@@ -48,7 +48,7 @@ export const INITIAL_DATA = [
   "thumbnail": "",
   "difficulty": "中等",
   "fun": "高",
-  "author": "ArenaMaster",
+  "author": "隨機單中：大混戰集錦",
   "stats": {
     "difficulty": "中等",
     "fun": "高"
@@ -74,7 +74,7 @@ export const INITIAL_DATA = [
   "thumbnail": "",
   "difficulty": "中等",
   "fun": "高",
-  "author": "ArenaMaster",
+  "author": "隨機單中：大混戰集錦",
   "stats": {
     "difficulty": "中等",
     "fun": "高"
@@ -97,7 +97,165 @@ export const INITIAL_DATA = [
   "thumbnail": "",
   "difficulty": "中等",
   "fun": "高",
-  "author": "ArenaMaster",
+  "author": "隨機單中：大混戰集錦",
+  "stats": {
+    "difficulty": "中等",
+    "fun": "高"
+  }
+},
+{
+  "id": 1765464406430,
+  "champion": "冷知識",
+  "title": "冷知識",
+  "description": "冷知識",
+  "tags": [
+    "冷知識"
+  ],
+  "augments": [
+    "冷知識"
+  ],
+  "items": [
+    "冷知識"
+  ],
+  "videoUrl": "https://www.facebook.com/loldaily2020/videos/4233405153567804/",
+  "thumbnail": "",
+  "difficulty": "中等",
+  "fun": "高",
+  "author": "隨機單中：大混戰集錦",
+  "stats": {
+    "difficulty": "中等",
+    "fun": "高"
+  }
+},
+{
+  "id": 1765464490671,
+  "champion": "",
+  "title": "改版後新彩色增幅評價",
+  "description": "改版後新彩色增幅評價",
+  "tags": [
+    "改版後新彩色增幅評價"
+  ],
+  "augments": [
+    "改版後新彩色增幅評價"
+  ],
+  "items": [
+    "改版後新彩色增幅評價"
+  ],
+  "videoUrl": "https://www.facebook.com/reel/4218238528413550",
+  "thumbnail": "",
+  "difficulty": "中等",
+  "fun": "高",
+  "author": "隨機單中：大混戰集錦",
+  "stats": {
+    "difficulty": "中等",
+    "fun": "高"
+  }
+},
+{
+  "id": 1765464618546,
+  "champion": "",
+  "title": "花劍評價",
+  "description": "花劍評價不建議選",
+  "tags": [
+    "花劍",
+    "破曉綻放之劍"
+  ],
+  "augments": [
+    "花劍評價"
+  ],
+  "items": [
+    "花劍",
+    "破曉綻放之劍"
+  ],
+  "videoUrl": "https://www.facebook.com/reel/832824599629282",
+  "thumbnail": "",
+  "difficulty": "中等",
+  "fun": "高",
+  "author": "隨機單中：大混戰集錦",
+  "stats": {
+    "difficulty": "中等",
+    "fun": "高"
+  }
+},
+{
+  "id": 1765464669673,
+  "champion": "",
+  "title": "潘朵拉盒子",
+  "description": "潘朵拉盒子",
+  "tags": [
+    "潘朵拉盒子"
+  ],
+  "augments": [
+    "潘朵拉盒子"
+  ],
+  "items": [
+    "潘朵拉盒子"
+  ],
+  "videoUrl": "https://www.facebook.com/reel/692285113765067",
+  "thumbnail": "",
+  "difficulty": "中等",
+  "fun": "高",
+  "author": "隨機單中：大混戰集錦",
+  "stats": {
+    "difficulty": "中等",
+    "fun": "高"
+  }
+},
+{
+  "id": 1765465629999,
+  "champion": "札克,茂凱,蒙多,魔鬥,烏迪爾,卡桑帝,薩亨,劍魔,達瑞斯,安比薩,弗力貝爾,伊羅旖,索拉卡,瑟拉芬,索娜,卡瑪,米里歐,銳空",
+  "title": "蛋白飲,蛋白粉奶昔",
+  "description": "坦克:札克,茂凱,蒙多,魔鬥,烏迪爾,卡桑帝\n出裝:無盡絕望,賈克修,好戰,振奮,蘭頓,自然之力\n\n物理系戰士:薩亨,劍魔,達瑞斯,安比薩,弗力貝爾,伊羅旖\n出裝:傲慢,突破天際,死亡之舞,魔提斯深淵,賈克修,振奮\n\n輔助:索拉卡,瑟拉芬,索娜,卡瑪,米里歐,銳空\n出裝:月石,日輪的加冕,贖罪神石,騎士誓願,米凱的魔鍋,賈克修",
+  "tags": [
+    "蛋白飲",
+    "蛋白粉奶昔",
+    "札克",
+    "茂凱",
+    "蒙多",
+    "魔鬥",
+    "烏迪爾",
+    "卡桑帝",
+    "薩亨",
+    "劍魔",
+    "達瑞斯",
+    "安比薩",
+    "弗力貝爾",
+    "伊羅旖",
+    "索拉卡",
+    "瑟拉芬",
+    "索娜",
+    "卡瑪",
+    "米里歐",
+    "銳空"
+  ],
+  "augments": [
+    "蛋白飲",
+    "蛋白粉奶昔",
+    "治療爆擊",
+    "全都給你"
+  ],
+  "items": [
+    "無盡絕望",
+    "賈克修",
+    "好戰",
+    "振奮",
+    "蘭頓",
+    "自然之力",
+    "傲慢",
+    "突破天際",
+    "死亡之舞",
+    "魔提斯深淵",
+    "月石",
+    "日輪的加冕",
+    "贖罪神石",
+    "騎士誓願",
+    "米凱的魔鍋"
+  ],
+  "videoUrl": "https://www.facebook.com/reel/2359780027804821",
+  "thumbnail": "",
+  "difficulty": "中等",
+  "fun": "高",
+  "author": "隨機單中：大混戰集錦",
   "stats": {
     "difficulty": "中等",
     "fun": "高"
